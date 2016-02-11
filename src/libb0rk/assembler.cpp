b3a5e6a1de238cc9548cb3dcdce64e76ef04dcd3
Ian Parker <ian@geekprojects.com>
2016-02-11 08:59:00 +0000

    Apply LGPL3 license and copyright headers

--- src/libb0rk/assembler.cpp
+++ src/libb0rk/assembler.cpp
@@ -1,3 +1,23 @@
+/*
+ *  b0rk - The b0rk Embeddable Runtime Environment
+ *  Copyright (C) 2015, 2016 GeekProjects.com
+ *
+ *  This file is part of b0rk.
+ *
+ *  b0rk is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  b0rk is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with b0rk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #undef DEBUG_ASSEMBLER
 
