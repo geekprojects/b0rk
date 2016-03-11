985d35980cb3755e215297b4a95b57c912350c52
Ian Parker <ian@geekprojects.com>
2016-03-11 18:35:40 +0000

    Travis: Move absolute CXX to top

--- build/travis.sh
+++ build/travis.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+export CC=`which $CC`
+export CXX=`which $CXX`
+
 git clone https://github.com/geekprojects/libgeek.git
 cd libgeek
 ./autogen.sh
 ./configure
 make
 
-export CC=`which $CC`
-export CXX=`which $CXX`
 sudo make install
 
